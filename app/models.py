78b41b4c35910d939462dad7ab31909d04b933f1
Temirov Azamat <azamatt04@mail.ru>
2023-04-30 00:20:51 +0600

    added company, genre, person and budget in models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class Company(m.Model):
+    name = m.CharField(max_length=45)
+    url = m.URLField()
+    preview_url = m.URLField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(m.Model):
+    name = m.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(m.Model):
+    photo = m.ImageField()
+    name = m.CharField(max_length=55)
+    profession = m.CharField(max_length=55)
+
+    def __str__(self):
+        return self.name
+
+
+class Budget(m.Model):
+    currency = m.CharField(max_length=2)
+
+    def __str__(self):
+        return self.currency
