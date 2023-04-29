8b1e7fefd1f530112d201ca75a305cb6889ae9da
Temirov Azamat <azamatt04@mail.ru>
2023-04-30 00:49:39 +0600

    updated company, genre, person and budget in models

--- app/models.py
+++ app/models.py
@@ -1,33 +1,33 @@
-from django.db import models as m
+from django.db import models
 
 
-class Company(m.Model):
-    name = m.CharField(max_length=45)
-    url = m.URLField()
-    preview_url = m.URLField(null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Genre(m.Model):
-    name = m.CharField(max_length=25)
+class Company(models.Model):
+    name = models.CharField(max_length=45)
+    url = models.URLField()
+    preview_url = models.URLField(null=True)
 
     def __str__(self):
         return self.name
 
 
-class Person(m.Model):
-    photo = m.ImageField()
-    name = m.CharField(max_length=55)
-    profession = m.CharField(max_length=55)
+class Genre(models.Model):
+    name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
 
 
-class Budget(m.Model):
-    currency = m.CharField(max_length=2)
+class Person(models.Model):
+    photo = models.ImageField()
+    name = models.CharField(max_length=55)
+    profession = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.name
+
+
+class Budget(models.Model):
+    currency = models.CharField(max_length=2)
 
     def __str__(self):
         return self.currency
