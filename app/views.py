dc04e6e85b6f5dadfe30b2e658db918d3d7c771b
Temirov Azamat <azamatt04@mail.ru>
2023-04-30 18:06:56 +0600

    added genre, person and budget im views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class GenreListCreateAPIView(generics.ListCreateAPIView):
+    queryset = models.Genre.objects.all()
+    serializer_class = serializers.GenreSerializer
+
+
+class PersonListCreateAPIView(generics.ListCreateAPIView):
+    queryset = models.Person.objects.all()
+    serializer_class = serializers.GenreSerializer
+
+
+class BudgetListCreateAPIView(generics.ListCreateAPIView):
+    queryset = models.Budget.objects.all()
+    serializer_class = serializers.BudgetSerializer
