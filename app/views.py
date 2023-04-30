3c0e4b0b2440255f3c0a66c0c11efc005a27644b
Quat228 <zharkulov04@mail.ru>
2023-04-30 18:05:31 +0600

    added views Movie and Rating - ListCreateAPIView

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class MovieListCreateAPIView(generics.ListCreateAPIView):
+    queryset = models.Movie.objects.all()
+    serializer_class = serializers.MovieSerializer
+
+
+class RatingListCreateAPIView(generics.ListCreateAPIView):
+    queryset = models.Rating.objects.all()
+    serializer_class = serializers.RatingSerializer
