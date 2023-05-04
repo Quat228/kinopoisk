c22a4f4f7593a3a9d1d25efbdd7db88ebddef28c
Temirov Azamat <azamatt04@mail.ru>
2023-05-04 14:00:10 +0600

    added swagger

--- app/views.py
+++ app/views.py
@@ -9,31 +9,54 @@ from . import serializers
 
 
 class FilmWorkListCreateAPIView(generics.ListAPIView):
+    """
+    FilmWork List and Create View
+    :param limit: int
+    """
     queryset = models.FilmWork.objects.all()
     serializer_class = serializers.FilmWorkSerializer
 
 
 class RatingListAPIView(generics.ListAPIView):
+    """
+    Rating List View
+    :param limit: int
+    """
     queryset = models.Rating.objects.all()
     serializer_class = serializers.RatingSerializer
 
 
 class GenreListAPIView(generics.ListAPIView):
+    """
+    Genre List View
+    :param limit: int
+    """
     queryset = models.Genre.objects.all()
     serializer_class = serializers.GenreSerializer
 
 
 class PersonListAPIView(generics.ListAPIView):
+    """
+    Person List View
+    :param limit: int
+    """
     queryset = models.Person.objects.all()
     serializer_class = serializers.PersonSerializer
 
 
 class CurrencyListAPIView(generics.ListAPIView):
+    """
+    Currency List View
+    :param limit: int
+    """
     queryset = models.Currency.objects.all()
     serializer_class = serializers.CurrencySerializer
 
 
 class FilmWorkRetrieveUpdateDestroyAPIView(views.APIView):
+    """
+    FilmWork Retrieve, Update and Destroy View
+    """
     def get_object(self, pk):
         return get_object_or_404(models.FilmWork, pk=pk)
 
